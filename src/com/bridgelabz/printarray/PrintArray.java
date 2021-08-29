a60d77a4f20c423673bb04dffb41522257078118
AshikaChadaga <ashikachadaga@gmail.com>
2021-08-29 09:59:29 +0530

    ADDED: Welcome message

---
+++ src/com/bridgelabz/printarray/PrintArray.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.printarray;
+
+public class PrintArray {
+	
+	public static void main(String[] args) {
+		System.out.println("----- Welcome to Printing Array using Java Generics -----");
+	}
+}
